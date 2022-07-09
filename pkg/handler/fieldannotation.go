9f87713a4461df693a6da02846a6086194853759
phelan_instill <nguyenvantamdk2@gmail.com>
2022-07-09 08:00:42 +0700

    chore: update protobuf message (#59)
    Because
    
    - protobuf message change bytes to content
    - error when trigger a pipeline with grpc stream method
    
    This commit
    
    - update required fields
    - fix read file length when trigger a pipeline

--- pkg/handler/fieldannotation.go
+++ pkg/handler/fieldannotation.go
@@ -7,7 +7,7 @@ var activateRequiredFields = []string{"name"}
 var deactivateRequiredFields = []string{"name"}
 var renameRequiredFields = []string{"name", "new_pipeline_id"}
 var triggerRequiredFields = []string{"name", "inputs"}
-var triggerBinaryRequiredFields = []string{"name", "file_lengths", "bytes"}
+var triggerBinaryRequiredFields = []string{"name", "file_lengths", "content"}
 
 // immutableFields are Protobuf message fields with IMMUTABLE field_behavior annotation
 var immutableFields = []string{"id", "recipe", "recipe.source", "recipe.model_instances", "recipe.destination"}
