aaf85ec83fc7a3ad65a76ef06418ffa2b0345971
Xiaofei Du <xiaofei.du@instill.tech>
2023-02-08 00:04:35 +0000

    chore: rename mgmt host

--- config/config.go
+++ config/config.go
@@ -94,7 +94,7 @@ type ModelBackendConfig struct {
 
 // MgmtBackendConfig related to mgmt-backend
 type MgmtBackendConfig struct {
-	Host      string `koanf:"host"`
+	AdminHost string `koanf:"adminhost"`
 	AdminPort int    `koanf:"adminport"`
 	HTTPS     struct {
 		Cert string `koanf:"cert"`
