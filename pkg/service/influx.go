3db3af7d1dec5cab4aa11b5646cbc8d65cb1352c
HR Wu <5631010+heiruwu@users.noreply.github.com>
2023-06-29 15:06:50 +0800

    chore: add temporary check if influxdb is present (#186)
    Because
    
    - cause fatal error if influxdb is not present
    
    This commit
    
    - add temporary check for influxdb, influxdb will be always present soon
    as `base` adopt new envs

--- pkg/service/influx.go
+++ pkg/service/influx.go
@@ -2,8 +2,14 @@ package service
 
 import (
 	"github.com/influxdata/influxdb-client-go/v2/api/write"
+
+	"github.com/instill-ai/pipeline-backend/config"
 )
 
 func (s *service) WriteNewDataPoint(p *write.Point) {
-	s.influxDBWriteClient.WritePoint(p)
+	// TODO: influxDB will be updated to requied instead of optional
+	// will not need to consider the case if influxdb is not present
+	if config.Config.Log.External {
+		s.influxDBWriteClient.WritePoint(p)
+	}
 }
