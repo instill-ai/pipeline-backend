e6899f2c7d3e808cc3bae710289f47fd2c619054
Chang, Hui-Tang <huitang.chang@instill.tech>
2024-03-11 03:46:13 -0500

    chore: optimize connector existence check when creating or updating a pipeline (#416)
    Because
    
    - There are many potential bugs in the connector existence check when
    creating or updating a pipeline.
    
    This commit
    
    - Optimizes the connector existence check when creating or updating a
    pipeline.

--- pkg/service/errors.go
+++ pkg/service/errors.go
@@ -4,6 +4,8 @@ import "errors"
 
 var ErrNoPermission = errors.New("no permission")
 var ErrNotFound = errors.New("not found")
+var ErrConnectorNameError = errors.New("connector name error")
+var ErrConnectorNotFound = errors.New("connector not found")
 var ErrUnauthenticated = errors.New("unauthenticated")
 var ErrRateLimiting = errors.New("rate limiting")
 var ErrCanNotTriggerNonLatestPipelineRelease = errors.New("can not trigger non-latest pipeline release")
