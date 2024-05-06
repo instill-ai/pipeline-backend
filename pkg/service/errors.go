15b446ab6b5925696e5d16a705ad0bb694396972
Juan Vallés <jvallesm@gmail.com>
2024-05-06 09:53:36 +0200

    feat: improve plaintext credential field error message

--- pkg/service/errors.go
+++ pkg/service/errors.go
@@ -1,6 +1,10 @@
 package service
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/instill-ai/x/errmsg"
+)
 
 var ErrNoPermission = errors.New("no permission")
 var ErrNotFound = errors.New("not found")
@@ -8,5 +12,11 @@ var ErrUnauthenticated = errors.New("unauthenticated")
 var ErrRateLimiting = errors.New("rate limiting")
 var ErrCanNotTriggerNonLatestPipelineRelease = errors.New("can not trigger non-latest pipeline release")
 var ErrExceedMaxBatchSize = errors.New("the batch size can not exceed 32")
-var ErrCanNotUsePlaintextSecret = errors.New("cannot use plaintext value for credential fields; must use secret reference")
 var ErrTriggerFail = errors.New("failed to trigger the pipeline")
+
+// ErrCanNotUsePlaintextSecret prevents users from adding connection details
+// with plaintext values.
+var ErrCanNotUsePlaintextSecret = errmsg.AddMessage(
+	errors.New("plaintext value in credential field"),
+	"Plaintext values are forbidden in credential fields. You can create a secret and reference it with the syntax ${secrets.my-secret}.",
+)
