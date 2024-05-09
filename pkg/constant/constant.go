848062b9de76f2b91aec4fb9b923a8cbff4e0932
Juan Vallés <jvallesm@gmail.com>
2024-05-09 09:16:48 +0200

    chore: replace credential references by secret

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -20,10 +20,10 @@ const HeaderAuthTypeKey = "Instill-Auth-Type"
 const HeaderInstillCodeKey = "Instill-Share-Code"
 const HeaderReturnTracesKey = "Instill-Return-Traces"
 
-// GlobalCredentialSecretKey can be used to reference a global secret
-// in the configuration of a component (i.e, ${secrets.INSTILL_CREDENTIAL}).
+// GlobalSecretKey can be used to reference a global secret in the
+// configuration of a component (i.e, ${secrets.INSTILL_SECRET}).
 //
 // Components can be configured to hold global secrets for certain parameters.
 // Given this configuration, a component will override the value of a parameter
 // with its global secret when it finds this keyword.
-const GlobalCredentialSecretKey = "INSTILL_CREDENTIAL"
+const GlobalSecretKey = "INSTILL_SECRET"
