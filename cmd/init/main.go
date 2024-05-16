f63000487bc8c2910aefca6db91f185153f7f398
Juan Vallés <jvallesm@gmail.com>
2024-05-16 09:01:36 +0200

    chore: remove unused types and imports

--- cmd/init/main.go
+++ cmd/init/main.go
@@ -3,53 +3,15 @@ package main
 import (
 	"context"
 	"log"
-	"time"
 
-	_ "embed"
-
-	"github.com/gofrs/uuid"
 	"github.com/redis/go-redis/v9"
 	"go.opentelemetry.io/otel"
-	"gorm.io/datatypes"
-	"gorm.io/gorm"
 
 	"github.com/instill-ai/pipeline-backend/cmd/init/definitionupdater"
 	"github.com/instill-ai/pipeline-backend/config"
 	database "github.com/instill-ai/pipeline-backend/pkg/db"
 	"github.com/instill-ai/pipeline-backend/pkg/repository"
 )
-
-type PrebuiltConnector struct {
-	ID                     string      `json:"id"`
-	UID                    string      `json:"uid"`
-	Owner                  string      `json:"owner"`
-	ConnectorDefinitionUID string      `json:"connector_definition_uid"`
-	Configuration          interface{} `json:"configuration"`
-	Task                   string      `json:"task"`
-}
-
-// BaseDynamic contains common columns for all tables with dynamic UUID as primary key generated when creating
-type BaseDynamic struct {
-	UID        uuid.UUID      `gorm:"type:uuid;primary_key;<-:create"` // allow read and create
-	CreateTime time.Time      `gorm:"autoCreateTime:nano"`
-	UpdateTime time.Time      `gorm:"autoUpdateTime:nano"`
-	DeleteTime gorm.DeletedAt `sql:"index"`
-}
-
-// Connector is the data model of the connector table
-type Connector struct {
-	BaseDynamic
-	ID                     string
-	Owner                  string
-	ConnectorDefinitionUID uuid.UUID
-	Description            string
-	Tombstone              bool
-	Configuration          datatypes.JSON `gorm:"type:jsonb"`
-	ConnectorType          string         `sql:"type:string"`
-	State                  string         `sql:"type:string"`
-	Visibility             string         `sql:"type:string"`
-	Task                   string         `sql:"type:string"`
-}
 
 func main() {
 	if err := config.Init(config.ParseConfigFlag()); err != nil {
