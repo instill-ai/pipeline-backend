49aed286b413c9e37b664e32e195c108d4e1adeb
ChunHao <64747455+chuang8511@users.noreply.github.com>
2024-10-07 17:57:42 +0100

    fix(text): fix the bug about the list position (#712)
    Because
    
    - the position of the chunk is not correct
    
    This commit
    
    - fix the bug about the position
    - exclude the test data in precommit
    - add the test code for position bug

--- pkg/component/operator/text/v0/markdown_splitter_test.go
+++ pkg/component/operator/text/v0/markdown_splitter_test.go
@@ -1,6 +1,7 @@
 package text
 
 import (
+	"os"
 	"testing"
 
 	"github.com/frankban/quicktest"
@@ -74,4 +75,77 @@ func Test_MarkdownSplitter(t *testing.T) {
 			c.Assert(len(chunks), quicktest.DeepEquals, testCase.outputLen)
 		})
 	}
+}
+
+func Test_PositionChecker(t *testing.T) {
+	c := quicktest.New(t)
+
+	testCases := []struct {
+		input        ChunkTextInput
+		fileName    string
+		outputChunks []ContentChunk
+	}{
+		{
+			input: ChunkTextInput{
+				Strategy: Strategy{
+					Setting: Setting{
+						ChunkMethod:  "Markdown",
+						ChunkSize:    800,
+						ChunkOverlap: 200,
+					},
+				},
+			},
+			fileName: "testdata/list_position_test.md",
+			outputChunks: []ContentChunk{
+				{
+					Chunk:                "# Progress Notes\n\nLUTS  \n=> **LUTS:Lower Urinary Tract Symptoms，表示有下泌尿道感染**",
+					ContentStartPosition: 17,
+					ContentEndPosition:   73,
+				},
+				{
+					Chunk:                "# 護理\n\n主訴有困難解尿情形，有尿液感但都解不出來   \n=> **符合有症狀的泌尿道感染之徵象或症狀:解尿困難或疼痛(dysuria)**",
+					ContentStartPosition: 82,
+					ContentEndPosition:   146,
+				},
+				{
+					Chunk:                "# 個案泌尿道感染判定\n\n\n1. 沒有導尿管或導尿管留置未超過2天\n2. 病患有UTI感染症狀:解尿困難或疼痛(dysuria)、Lower Urinary Tract Symptoms  \n=> **判斷為:非導尿管相關泌尿道感染(Non-CAUTI)**\n\n",
+					ContentStartPosition: 160,
+					ContentEndPosition:   275,
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+
+		c.Run("Test bug cases reported about position", func(c *quicktest.C) {
+			inputStruct := testCase.input
+
+			bytes, err := os.ReadFile(testCase.fileName)
+			c.Assert(err, quicktest.IsNil)
+
+			inputStruct.Text = string(bytes)
+
+			setting := inputStruct.Strategy.Setting
+			split := NewMarkdownTextSplitter(
+				setting.ChunkSize,
+				setting.ChunkOverlap,
+				inputStruct.Text,
+			)
+
+			err = split.Validate()
+			c.Assert(err, quicktest.IsNil)
+
+			chunks, err := split.SplitText()
+
+			c.Assert(err, quicktest.IsNil)
+
+			for i, chunk := range chunks {
+				c.Assert(chunk.Chunk, quicktest.Equals, testCase.outputChunks[i].Chunk)
+				c.Assert(chunk.ContentStartPosition, quicktest.Equals, testCase.outputChunks[i].ContentStartPosition)
+				c.Assert(chunk.ContentEndPosition, quicktest.Equals, testCase.outputChunks[i].ContentEndPosition)
+			}
+		})
+	}
+
 }
---
+++ pkg/component/operator/text/v0/testdata/list_position_test.md
@@ -0,0 +1,12 @@
+# Progress Notes
+LUTS  
+=> **LUTS:Lower Urinary Tract Symptoms，表示有下泌尿道感染** 
+
+# 護理
+主訴有困難解尿情形，有尿液感但都解不出來   
+=> **符合有症狀的泌尿道感染之徵象或症狀:解尿困難或疼痛(dysuria)**
+
+# 個案泌尿道感染判定
+1. 沒有導尿管或導尿管留置未超過2天
+2. 病患有UTI感染症狀:解尿困難或疼痛(dysuria)、Lower Urinary Tract Symptoms  
+=> **判斷為:非導尿管相關泌尿道感染(Non-CAUTI)**
