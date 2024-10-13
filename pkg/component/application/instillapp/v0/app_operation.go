6def4acac0148f5dfd873f477a05e70337389559
ChunHao <64747455+chuang8511@users.noreply.github.com>
2024-10-13 12:10:25 +0100

    feat(component): implement instill app features (#670)
    Because
    
    - instill app is only exposed to ee version
    
    This commit
    
    - exclude instill app when it is ce version
    - initialise Instill App
    - fix the bug in Instill App when end to end test
    - chore the exported and non-exported functions
    
    Note
    - We should extend the OneOf function for output in the other PR in near
    future.

--- pkg/component/application/instillapp/v0/app_operation.go
+++ pkg/component/application/instillapp/v0/app_operation.go
@@ -13,9 +13,10 @@ import (
 	appPB "github.com/instill-ai/protogen-go/app/app/v1alpha"
 )
 
+// ReadChatHistoryInput is the input struct for the ReadChatHistory operation
 type ReadChatHistoryInput struct {
 	Namespace       string `json:"namespace"`
-	AppUID          string `json:"app-id"`
+	AppID           string `json:"app-id"`
 	ConversationID  string `json:"conversation-id"`
 	Role            string `json:"role"`
 	MessageType     string `json:"message-type"`
@@ -23,16 +24,22 @@ type ReadChatHistoryInput struct {
 	MaxMessageCount int    `json:"max-message-count"`
 }
 
+// ReadChatHistoryOutput is the output struct for the ReadChatHistory operation
 type ReadChatHistoryOutput struct {
 	Messages []Message `json:"messages"`
 }
 
+// Message is the struct for a message
 type Message struct {
 	Content []Content `json:"content"`
-	Role    string    `json:"role"`
-	Name    string    `json:"name,omitempty"`
+	// Role can be either 'user' or 'assistant'
+	Role string `json:"role"`
+	Name string `json:"name,omitempty"`
 }
 
+// Content is the struct for the content of a message.
+// It can be either text, image_url, or image_base64.
+// Only one of the fields should be set, and Type should be set to the type of the content.
 type Content struct {
 	Type        string `json:"type"`
 	Text        string `json:"text,omitempty"`
@@ -40,7 +47,7 @@ type Content struct {
 	ImageBase64 string `json:"image-base64,omitempty"`
 }
 
-func (in *ReadChatHistoryInput) Validate() error {
+func (in *ReadChatHistoryInput) validate() error {
 	if in.Role != "" && in.Role != "user" && in.Role != "assistant" {
 		return fmt.Errorf("role must be either 'user' or 'assistant'")
 	}
@@ -58,7 +65,7 @@ func (in *ReadChatHistoryInput) Validate() error {
 	return nil
 }
 
-func (out *ReadChatHistoryOutput) Filter(inputStruct ReadChatHistoryInput, messages []*appPB.Message) {
+func (out *ReadChatHistoryOutput) filter(inputStruct ReadChatHistoryInput, messages []*appPB.Message) {
 	for _, message := range messages {
 
 		if inputStruct.Role != "" && inputStruct.Role != message.Role {
@@ -81,7 +88,7 @@ func (out *ReadChatHistoryOutput) Filter(inputStruct ReadChatHistoryInput, messa
 
 		content := []Content{
 			{
-				Type: message.Type.String(),
+				Type: convertPBTypeToJSONType(message.Type.String()),
 				Text: message.Content,
 			},
 		}
@@ -90,6 +97,15 @@ func (out *ReadChatHistoryOutput) Filter(inputStruct ReadChatHistoryInput, messa
 			Content: content,
 			Role:    message.Role,
 		})
+	}
+}
+
+func convertPBTypeToJSONType(pbType string) string {
+	switch pbType {
+	case "MESSAGE_TYPE_TEXT":
+		return "text"
+	default:
+		return "unknown"
 	}
 }
 
@@ -102,7 +118,7 @@ func (e *execution) readChatHistory(input *structpb.Struct) (*structpb.Struct, e
 		return nil, fmt.Errorf("failed to convert input struct: %w", err)
 	}
 
-	err = inputStruct.Validate()
+	err = inputStruct.validate()
 	if err != nil {
 		return nil, fmt.Errorf("invalid input: %w", err)
 	}
@@ -117,7 +133,7 @@ func (e *execution) readChatHistory(input *structpb.Struct) (*structpb.Struct, e
 
 	res, err := appClient.ListMessages(ctx, &appPB.ListMessagesRequest{
 		NamespaceId:           inputStruct.Namespace,
-		AppId:                 inputStruct.AppUID,
+		AppId:                 inputStruct.AppID,
 		ConversationId:        inputStruct.ConversationID,
 		IncludeSystemMessages: true,
 	})
@@ -130,12 +146,12 @@ func (e *execution) readChatHistory(input *structpb.Struct) (*structpb.Struct, e
 		Messages: make([]Message, 0),
 	}
 
-	output.Filter(inputStruct, res.Messages)
+	output.filter(inputStruct, res.Messages)
 
 	for res.NextPageToken != "" || (len(output.Messages) < inputStruct.MaxMessageCount && inputStruct.MaxMessageCount > 0) {
 		res, err = appClient.ListMessages(ctx, &appPB.ListMessagesRequest{
 			NamespaceId:           inputStruct.Namespace,
-			AppId:                 inputStruct.AppUID,
+			AppId:                 inputStruct.AppID,
 			ConversationId:        inputStruct.ConversationID,
 			IncludeSystemMessages: true,
 			PageToken:             res.NextPageToken,
@@ -145,7 +161,7 @@ func (e *execution) readChatHistory(input *structpb.Struct) (*structpb.Struct, e
 			return nil, fmt.Errorf("failed to list messages: %w", err)
 		}
 
-		output.Filter(inputStruct, res.Messages)
+		output.filter(inputStruct, res.Messages)
 	}
 
 	return base.ConvertToStructpb(output)
@@ -154,7 +170,7 @@ func (e *execution) readChatHistory(input *structpb.Struct) (*structpb.Struct, e
 
 type WriteChatMessageInput struct {
 	Namespace      string       `json:"namespace"`
-	AppUID         string       `json:"app-id"`
+	AppID          string       `json:"app-id"`
 	ConversationID string       `json:"conversation-id"`
 	Message        WriteMessage `json:"message"`
 }
@@ -170,7 +186,7 @@ type WriteChatMessageOutput struct {
 	UpdateTime string `json:"update-time"`
 }
 
-func (in *WriteChatMessageInput) Validate() error {
+func (in *WriteChatMessageInput) validate() error {
 	role := in.Message.Role
 	if role != "" && role != "user" && role != "assistant" {
 		return fmt.Errorf("role must be either 'user' or 'assistant'")
@@ -186,7 +202,7 @@ func (e *execution) writeChatMessage(input *structpb.Struct) (*structpb.Struct, 
 		return nil, fmt.Errorf("failed to convert input struct: %w", err)
 	}
 
-	err = inputStruct.Validate()
+	err = inputStruct.validate()
 
 	if err != nil {
 		return nil, fmt.Errorf("invalid input: %w", err)
@@ -200,20 +216,57 @@ func (e *execution) writeChatMessage(input *structpb.Struct) (*structpb.Struct, 
 
 	ctx = metadata.NewOutgoingContext(ctx, getRequestMetadata(e.SystemVariables))
 
+	apps, err := appClient.ListApps(ctx, &appPB.ListAppsRequest{
+		NamespaceId: inputStruct.Namespace,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to list apps: %w", err)
+	}
+
+	found := false
+
+	for _, app := range apps.Apps {
+		if app.AppId == inputStruct.AppID {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		_, err = appClient.CreateApp(ctx, &appPB.CreateAppRequest{
+			NamespaceId: inputStruct.Namespace,
+			Id:          inputStruct.AppID,
+		})
+
+		if err != nil {
+			return nil, fmt.Errorf("failed to create app: %w", err)
+		}
+	}
+
 	conversations, err := appClient.ListConversations(ctx, &appPB.ListConversationsRequest{
-		NamespaceId:    inputStruct.Namespace,
-		AppId:          inputStruct.AppUID,
-		ConversationId: inputStruct.ConversationID,
+		NamespaceId: inputStruct.Namespace,
+		AppId:       inputStruct.AppID,
+		IfAll:       true,
 	})
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to list conversations: %w", err)
 	}
 
-	if len(conversations.Conversations) == 0 {
+	found = false
+
+	for _, conversation := range conversations.Conversations {
+		if conversation.Id == inputStruct.ConversationID {
+			found = true
+			break
+		}
+	}
+
+	if !found {
 		_, err = appClient.CreateConversation(ctx, &appPB.CreateConversationRequest{
 			NamespaceId:    inputStruct.Namespace,
-			AppId:          inputStruct.AppUID,
+			AppId:          inputStruct.AppID,
 			ConversationId: inputStruct.ConversationID,
 		})
 
@@ -224,7 +277,7 @@ func (e *execution) writeChatMessage(input *structpb.Struct) (*structpb.Struct, 
 
 	res, err := appClient.CreateMessage(ctx, &appPB.CreateMessageRequest{
 		NamespaceId:    inputStruct.Namespace,
-		AppId:          inputStruct.AppUID,
+		AppId:          inputStruct.AppID,
 		ConversationId: inputStruct.ConversationID,
 		Role:           inputStruct.Message.Role,
 		Type:           appPB.Message_MessageType(appPB.Message_MessageType_value["MESSAGE_TYPE_TEXT"]),
