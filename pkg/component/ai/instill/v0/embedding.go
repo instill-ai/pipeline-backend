b7a23426a98aea01db148184a4289226e40edf0a
chuang8511 <a0961192312@gmail.com>
2024-10-14 17:45:31 +0100

    feat(instillmodel): add embedding implementation

--- pkg/component/ai/instill/v0/embedding.go
+++ pkg/component/ai/instill/v0/embedding.go
@@ -1,11 +1,37 @@
 package instill
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/structpb"
 
 	modelPB "github.com/instill-ai/protogen-go/model/model/v1alpha"
 )
 
 func (e *execution) executeEmbedding(grpcClient modelPB.ModelPublicServiceClient, nsID string, modelID string, version string, inputs []*structpb.Struct) ([]*structpb.Struct, error) {
-	return nil, nil
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	ctx = metadata.NewOutgoingContext(ctx, getRequestMetadata(e.SystemVariables))
+
+	res, err := grpcClient.TriggerNamespaceModel(ctx, &modelPB.TriggerNamespaceModelRequest{
+		NamespaceId: nsID,
+		ModelId:     modelID,
+		Version:     version,
+		TaskInputs:  inputs,
+	})
+
+	if err != nil || res == nil {
+		return nil, fmt.Errorf("error triggering model: %v", err)
+	}
+
+	if len(res.TaskOutputs) > 0 {
+		return res.TaskOutputs, nil
+	}
+
+	return nil, fmt.Errorf("no output from model")
 }
