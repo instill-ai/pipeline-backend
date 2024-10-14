168bf7116654438de8a061827e1cdc914d6d7977
chuang8511 <a0961192312@gmail.com>
2024-10-14 16:37:35 +0100

    chore(migration): update migration

--- pkg/db/migration/convert/convert000032/convert.go
+++ pkg/db/migration/convert/convert000032/convert.go
@@ -2,9 +2,9 @@ package convert000032
 
 import (
 	"fmt"
-	"strings"
 
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
 
 	"github.com/instill-ai/pipeline-backend/pkg/datamodel"
@@ -29,14 +29,27 @@ func (c *ConvertToWeb) migratePipeline() error {
 	pipelines := make([]*datamodel.Pipeline, 0, batchSize)
 	return c.DB.Select("uid", "recipe_yaml", "recipe").FindInBatches(&pipelines, batchSize, func(tx *gorm.DB, _ int) error {
 		for _, p := range pipelines {
+			isRecipeUpdated := false
 			l := c.Logger.With(zap.String("pipelineUID", p.UID.String()))
 
-			recipeYAML := p.RecipeYAML
+			for id, comp := range p.Recipe.Component {
+				isComponentUpdated, err := c.update(comp)
+				if err != nil {
+					l.With(zap.String("componentID", id), zap.Error(err)).
+						Error("Failed to update pipeline.")
 
-			updatedRecipeYAML, isUpdate := c.updateRecipeYAML(recipeYAML)
+					return fmt.Errorf("updating pipeline component: %w", err)
+				}
 
-			if isUpdate {
-				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", updatedRecipeYAML)
+				isRecipeUpdated = isComponentUpdated || isRecipeUpdated
+			}
+
+			if isRecipeUpdated {
+				recipeYAML, err := yaml.Marshal(p.Recipe)
+				if err != nil {
+					return fmt.Errorf("marshalling recipe: %w", err)
+				}
+				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", string(recipeYAML))
 				if result.Error != nil {
 					l.Error("Failed to update pipeline release.")
 					return fmt.Errorf("updating pipeline recipe: %w", result.Error)
@@ -49,17 +62,30 @@ func (c *ConvertToWeb) migratePipeline() error {
 }
 
 func (c *ConvertToWeb) migratePipelineRelease() error {
-	pipelineReleases := make([]*datamodel.PipelineRelease, 0, batchSize)
-	return c.DB.Select("uid", "recipe_yaml", "recipe").FindInBatches(&pipelineReleases, batchSize, func(tx *gorm.DB, _ int) error {
-		for _, p := range pipelineReleases {
-			l := c.Logger.With(zap.String("pipelineUID", p.UID.String()))
+	pipelines := make([]*datamodel.Pipeline, 0, batchSize)
+	return c.DB.Select("uid", "recipe_yaml", "recipe").FindInBatches(&pipelines, batchSize, func(tx *gorm.DB, _ int) error {
+		for _, p := range pipelines {
+			isRecipeUpdated := false
+			l := c.Logger.With(zap.String("pipelineReleaseUID", p.UID.String()))
 
-			recipeYAML := p.RecipeYAML
+			for id, comp := range p.Recipe.Component {
+				isComponentUpdated, err := c.update(comp)
+				if err != nil {
+					l.With(zap.String("componentID", id), zap.Error(err)).
+						Error("Failed to update pipeline.")
 
-			updatedRecipeYAML, isUpdate := c.updateRecipeYAML(recipeYAML)
+					return fmt.Errorf("updating pipeline component: %w", err)
+				}
 
-			if isUpdate {
-				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", updatedRecipeYAML)
+				isRecipeUpdated = isComponentUpdated || isRecipeUpdated
+			}
+
+			if isRecipeUpdated {
+				recipeYAML, err := yaml.Marshal(p.Recipe)
+				if err != nil {
+					return fmt.Errorf("marshalling recipe: %w", err)
+				}
+				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", string(recipeYAML))
 				if result.Error != nil {
 					l.Error("Failed to update pipeline release.")
 					return fmt.Errorf("updating pipeline recipe: %w", result.Error)
@@ -71,24 +97,60 @@ func (c *ConvertToWeb) migratePipelineRelease() error {
 	}).Error
 }
 
-func (c *ConvertToWeb) updateRecipeYAML(recipeYAML string) (updatedRecipeYAML string, isUpdate bool) {
+func (c *ConvertToWeb) update(comp *datamodel.Component) (bool, error) {
 
-	if !strings.Contains(recipeYAML, "TASK_CRAWL_WEBSITE") && !strings.Contains(recipeYAML, "TASK_SCRAPE_WEBPAGE") {
-		return "", false
+	if comp.Type == "iterator" {
+		isComponentUpdated := false
+		for _, comp := range comp.Component {
+			isUpdated, err := c.update(comp)
+			if err != nil {
+				return false, err
+			}
+			isComponentUpdated = isUpdated || isComponentUpdated
+		}
+		return isComponentUpdated, nil
 	}
 
-	if strings.Contains(recipeYAML, "TASK_CRAWL_WEBSITE") {
-		// Insert the description of the recipe needs to be updated in the top of the YAML file
-		updatedRecipeYAML = fmt.Sprintf("# Recipe updated to replace TASK_CRAWL_WEBSITE with TASK_CRAWL_SITE and deprecated some fields. \n# Please check the latest document to update your recipe.\n%s", recipeYAML)
-
-		updatedRecipeYAML = strings.ReplaceAll(updatedRecipeYAML, "TASK_CRAWL_WEBSITE", "TASK_CRAWL_SITE")
-		isUpdate = true
+	if comp.Type != "web" {
+		return false, nil
 	}
 
-	if strings.Contains(recipeYAML, "TASK_SCRAPE_WEBPAGE") {
-		updatedRecipeYAML = strings.ReplaceAll(recipeYAML, "TASK_SCRAPE_WEBPAGE", "TASK_SCRAPE_PAGE")
-		isUpdate = true
+	if comp.Task != "TASK_CRAWL_WEBSITE" && comp.Task != "TASK_SCRAPE_WEBPAGE" {
+		return false, nil
 	}
 
-	return updatedRecipeYAML, isUpdate
+	if comp.Task == "TASK_CRAWL_WEBSITE" {
+		comp.Task = "TASK_CRAWL_SITE"
+		toRemoveFields := []string{
+			"include-link-html",
+			"include-link-text",
+			"only-main-content",
+			"remove-tags",
+			"only-include-tags",
+		}
+
+		input, isMap := comp.Input.(map[string]interface{})
+
+		if !isMap {
+			return false, nil
+		}
+
+		for _, field := range toRemoveFields {
+			delete(input, field)
+		}
+
+		if v, ok := input["target-url"]; ok {
+			input["url"] = v
+			delete(input, "target-url")
+		}
+		return true, nil
+	}
+
+	if comp.Task == "TASK_SCRAPE_WEBPAGE" {
+		comp.Task = "TASK_SCRAPE_PAGE"
+		return true, nil
+	}
+
+	return false, nil
+
 }
