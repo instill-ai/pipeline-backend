81fd8f7c875e5f3031770cc74f329dfafced8ff3
chuang8511 <a0961192312@gmail.com>
2024-10-17 22:39:54 +0100

    chore(instillmodel): add comments

--- pkg/component/ai/embedding.go
+++ pkg/component/ai/embedding.go
@@ -1,39 +1,63 @@
 package ai
 
+// EmbeddingInput is the standardized input for the embedding model.
 type EmbeddingInput struct {
+	// Data is the the standardized input data for the embedding model.
 	Data      EmbeddingInputData `json:"data"`
+	// Parameter is the standardized parameter for the embedding model.
 	Parameter EmbeddingParameter `json:"parameter"`
 }
 
+// EmbeddingInputData is the standardized input data for the embedding model.
 type EmbeddingInputData struct {
+	// Model is the model name.
 	Model      string           `json:"model"`
+	// Embeddings is the list of data to be embedded.
 	Embeddings []InputEmbedding `json:"embeddings"`
 }
 
+// InputEmbedding is the standardized input data to be embedded.
 type InputEmbedding struct {
+	// Type is the type of the input data. It can be either "text", "image-url", or "image-base64".
 	Type        string `json:"type"`
+	// Text is the text to be embedded.
 	Text        string `json:"text"`
+	// ImageURL is the URL of the image to be embedded.
 	ImageURL    string `json:"image-url"`
+	// ImageBase64 is the base64 encoded image to be embedded.
 	ImageBase64 string `json:"image-base64"`
 }
 
+// EmbeddingParameter is the standardized parameter for the embedding model.
 type EmbeddingParameter struct {
+	// Format is the format of the output embeddings. Default is "float", can be "float" or "base64".
 	Format     string `json:"format"`
+	// Dimensions is the number of dimensions of the output embeddings.
 	Dimensions int    `json:"dimensions"`
+	// InputType is the type of the input data. It can be "query" or "data".
 	InputType  string `json:"input-type"`
+	// Truncate is how to handle inputs longer than the max token length. Defaults to 'End'. Can be 'End', 'Start', or 'None'.
 	Truncate   string `json:"truncate"`
 }
 
+// EmbeddingOutput is the standardized output for the embedding model.
 type EmbeddingOutput struct {
+	// Data is the standardized output data for the embedding model.
 	Data EmbeddingOutputData `json:"data"`
 }
 
+// EmbeddingOutputData is the standardized output data for the embedding model.
 type EmbeddingOutputData struct {
+	// Embeddings is the list of output embeddings.
 	Embeddings []OutputEmbedding `json:"embeddings"`
 }
 
+// OutputEmbedding is the standardized output embedding.
 type OutputEmbedding struct {
+	// Index is the index of the output embedding.
 	Index   int   `json:"index"`
+	// Vector is the output embedding.
 	Vector  []any `json:"vector"`
+	// Created is the Unix timestamp (in seconds) of when the embedding was created.
 	Created int   `json:"created"`
 }
--- pkg/component/ai/instill/v0/embedding.go
+++ pkg/component/ai/instill/v0/embedding.go
@@ -8,17 +8,17 @@ import (
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/structpb"
 
-	modelPB "github.com/instill-ai/protogen-go/model/model/v1alpha"
+	modelpb "github.com/instill-ai/protogen-go/model/model/v1alpha"
 )
 
-func (e *execution) executeEmbedding(grpcClient modelPB.ModelPublicServiceClient, nsID string, modelID string, version string, inputs []*structpb.Struct) ([]*structpb.Struct, error) {
+func (e *execution) executeEmbedding(grpcClient modelpb.ModelPublicServiceClient, nsID string, modelID string, version string, inputs []*structpb.Struct) ([]*structpb.Struct, error) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	ctx = metadata.NewOutgoingContext(ctx, getRequestMetadata(e.SystemVariables))
 
-	res, err := grpcClient.TriggerNamespaceModel(ctx, &modelPB.TriggerNamespaceModelRequest{
+	res, err := grpcClient.TriggerNamespaceModel(ctx, &modelpb.TriggerNamespaceModelRequest{
 		NamespaceId: nsID,
 		ModelId:     modelID,
 		Version:     version,
