95feb6dedd3c95ac07e2828b28ba1d2f71dd6a70
Naila Rais <157850227+NailaRais@users.noreply.github.com>
2024-10-18 20:15:18 +0530

    Update main.go

--- pkg/component/operator/text/v0/main.go
+++ pkg/component/operator/text/v0/main.go
@@ -4,6 +4,8 @@ package text
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strings"
 	"sync"
 
 	_ "embed"
@@ -12,7 +14,8 @@ import (
 )
 
 const (
-	taskChunkText string = "TASK_CHUNK_TEXT"
+	taskChunkText    string = "TASK_CHUNK_TEXT"
+	taskDataCleansing string = "TASK_DATA_CLEANSING"
 )
 
 var (
@@ -52,6 +55,122 @@ func (c *component) CreateExecution(x base.ComponentExecution) (base.IExecution,
 	return &execution{ComponentExecution: x}, nil
 }
 
+// CleanDataInput defines the input structure for the data cleansing task
+type CleanDataInput struct {
+	Texts   []string             `json:"texts"`   // Array of text to be cleaned
+	Setting DataCleaningSetting  `json:"setting"` // Cleansing configuration
+}
+
+// CleanDataOutput defines the output structure for the data cleansing task
+type CleanDataOutput struct {
+	CleanedTexts []string `json:"texts"` // Array of cleaned text
+}
+
+// DataCleaningSetting defines the configuration for data cleansing
+type DataCleaningSetting struct {
+	CleanMethod     string   `json:"clean-method"` // "Regex" or "Substring"
+	ExcludePatterns []string `json:"exclude-patterns,omitempty"`
+	IncludePatterns []string `json:"include-patterns,omitempty"`
+	ExcludeSubstrs  []string `json:"exclude-substrings,omitempty"`
+	IncludeSubstrs  []string `json:"include-substrings,omitempty"`
+	CaseSensitive   bool     `json:"case-sensitive,omitempty"`
+}
+
+// CleanData cleans the input texts based on the provided settings
+func CleanData(input CleanDataInput) CleanDataOutput {
+	var cleanedTexts []string
+
+	switch input.Setting.CleanMethod {
+	case "Regex":
+		cleanedTexts = cleanTextUsingRegex(input.Texts, input.Setting)
+	case "Substring":
+		cleanedTexts = cleanTextUsingSubstring(input.Texts, input.Setting)
+	default:
+		// If no valid method is provided, return the original texts
+		cleanedTexts = input.Texts
+	}
+
+	return CleanDataOutput{CleanedTexts: cleanedTexts}
+}
+
+// cleanTextUsingRegex cleans the input texts using regular expressions based on the given settings
+func cleanTextUsingRegex(inputTexts []string, settings DataCleaningSetting) []string {
+	var cleanedTexts []string
+
+	for _, text := range inputTexts {
+		include := true
+
+		// Exclude patterns
+		for _, pattern := range settings.ExcludePatterns {
+			re := regexp.MustCompile(pattern)
+			if re.MatchString(text) {
+				include = false
+				break
+			}
+		}
+
+		// Include patterns
+		if include && len(settings.IncludePatterns) > 0 {
+			include = false
+			for _, pattern := range settings.IncludePatterns {
+				re := regexp.MustCompile(pattern)
+				if re.MatchString(text) {
+					include = true
+					break
+				}
+			}
+		}
+
+		if include {
+			cleanedTexts = append(cleanedTexts, text)
+		}
+	}
+	return cleanedTexts
+}
+
+// cleanTextUsingSubstring cleans the input texts using substrings based on the given settings
+func cleanTextUsingSubstring(inputTexts []string, settings DataCleaningSetting) []string {
+	var cleanedTexts []string
+
+	for _, text := range inputTexts {
+		include := true
+		compareText := text
+		if !settings.CaseSensitive {
+			compareText = strings.ToLower(text)
+		}
+
+		// Exclude substrings
+		for _, substr := range settings.ExcludeSubstrs {
+			if !settings.CaseSensitive {
+				substr = strings.ToLower(substr)
+			}
+			if strings.Contains(compareText, substr) {
+				include = false
+				break
+			}
+		}
+
+		// Include substrings
+		if include && len(settings.IncludeSubstrs) > 0 {
+			include = false
+			for _, substr := range settings.IncludeSubstrs {
+				if !settings.CaseSensitive {
+					substr = strings.ToLower(substr)
+				}
+				if strings.Contains(compareText, substr) {
+					include = true
+					break
+				}
+			}
+		}
+
+		if include {
+			cleanedTexts = append(cleanedTexts, text)
+		}
+	}
+	return cleanedTexts
+}
+
 // Execute executes the derived execution
 func (e *execution) Execute(ctx context.Context, jobs []*base.Job) error {
 
@@ -82,6 +201,25 @@ func (e *execution) Execute(ctx context.Context, jobs []*base.Job) error {
 				continue
 			}
 			output, err := base.ConvertToStructpb(outputStruct)
+			if err != nil {
+				job.Error.Error(ctx, err)
+				continue
+			}
+			err = job.Output.Write(ctx, output)
+			if err != nil {
+				job.Error.Error(ctx, err)
+				continue
+			}
+		case taskDataCleansing:
+			cleanDataInput := CleanDataInput{}
+			err := base.ConvertFromStructpb(input, &cleanDataInput)
+			if err != nil {
+				job.Error.Error(ctx, err)
+				continue
+			}
+
+			cleanedDataOutput := CleanData(cleanDataInput)
+			output, err := base.ConvertToStructpb(cleanedDataOutput)
 			if err != nil {
 				job.Error.Error(ctx, err)
 				continue
