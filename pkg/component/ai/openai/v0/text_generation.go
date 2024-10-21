5950c80bf01d8226cb59f412e4f50ee2862e0619
Chang, Hui-Tang <huitang.chang@instill.tech>
2024-10-21 16:28:57 +0800

    chore(openai): use new Read/WriteData interface for text generation

--- pkg/component/ai/openai/v0/text_generation.go
+++ pkg/component/ai/openai/v0/text_generation.go
@@ -9,32 +9,6 @@ type TextMessage struct {
 	Content []Content `json:"content"`
 }
 
-type TextCompletionInput struct {
-	Prompt           string                     `json:"prompt"`
-	Images           []string                   `json:"images"`
-	ChatHistory      []*TextMessage             `json:"chat-history,omitempty"`
-	Model            string                     `json:"model"`
-	SystemMessage    *string                    `json:"system-message,omitempty"`
-	Temperature      *float32                   `json:"temperature,omitempty"`
-	TopP             *float32                   `json:"top-p,omitempty"`
-	N                *int                       `json:"n,omitempty"`
-	Stop             *string                    `json:"stop,omitempty"`
-	MaxTokens        *int                       `json:"max-tokens,omitempty"`
-	PresencePenalty  *float32                   `json:"presence-penalty,omitempty"`
-	FrequencyPenalty *float32                   `json:"frequency-penalty,omitempty"`
-	ResponseFormat   *responseFormatInputStruct `json:"response-format,omitempty"`
-}
-
-type responseFormatInputStruct struct {
-	Type       string `json:"type,omitempty"`
-	JSONSchema string `json:"json-schema,omitempty"`
-}
-
-type TextCompletionOutput struct {
-	Texts []string `json:"texts"`
-	Usage usage    `json:"usage"`
-}
-
 type textCompletionReq struct {
 	Model            string                   `json:"model"`
 	Messages         []interface{}            `json:"messages"`
@@ -115,10 +89,4 @@ type usageOpenAI struct {
 	PromptTokens     int `json:"prompt_tokens"`
 	CompletionTokens int `json:"completion_tokens"`
 	TotalTokens      int `json:"total_tokens"`
-}
-
-type usage struct {
-	PromptTokens     int `json:"prompt-tokens"`
-	CompletionTokens int `json:"completion-tokens"`
-	TotalTokens      int `json:"total-tokens"`
 }
