e5a62b40c56aafc4b40695d585be41a8c9b92807
Chang, Hui-Tang <huitang.chang@instill.tech>
2024-10-21 21:59:08 +0800

    update openai tasks

--- pkg/component/ai/openai/v0/text_embeddings.go
+++ pkg/component/ai/openai/v0/text_embeddings.go
@@ -4,16 +4,6 @@ const (
 	embeddingsPath = "/v1/embeddings"
 )
 
-type TextEmbeddingsInput struct {
-	Text       string `json:"text"`
-	Model      string `json:"model"`
-	Dimensions int    `json:"dimensions"`
-}
-
-type TextEmbeddingsOutput struct {
-	Embedding []float64 `json:"embedding"`
-}
-
 type TextEmbeddingsReq struct {
 	Model      string   `json:"model"`
 	Dimensions int      `json:"dimensions,omitempty"`
@@ -29,6 +19,6 @@ type TextEmbeddingsResp struct {
 
 type Data struct {
 	Object    string    `json:"object"`
-	Embedding []float64 `json:"embedding"`
+	Embedding []float32 `json:"embedding"`
 	Index     int       `json:"index"`
 }
--- pkg/component/ai/openai/v0/text_to_speech.go
+++ pkg/component/ai/openai/v0/text_to_speech.go
@@ -4,22 +4,10 @@ const (
 	createSpeechPath = "/v1/audio/speech"
 )
 
-type TextToSpeechInput struct {
-	Text           string   `json:"text"`
-	Model          string   `json:"model"`
-	Voice          string   `json:"voice"`
-	ResponseFormat *string  `json:"response-format,omitempty"`
-	Speed          *float64 `json:"speed,omitempty"`
-}
-
-type TextToSpeechOutput struct {
-	Audio string `json:"audio"`
-}
-
 type TextToSpeechReq struct {
 	Input          string   `json:"input"`
 	Model          string   `json:"model"`
 	Voice          string   `json:"voice"`
 	ResponseFormat *string  `json:"response_format,omitempty"`
-	Speed          *float64 `json:"speed,omitempty"`
+	Speed          *float32 `json:"speed,omitempty"`
 }
