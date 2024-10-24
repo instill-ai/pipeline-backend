e2674eb0434bf6d3961a5083ef0b207292315d31
chuang8511 <a0961192312@gmail.com>
2024-10-24 12:21:55 +0100

    fix(web): fix migration bug

--- pkg/db/migration/convert/convert000033/convert.go
+++ pkg/db/migration/convert/convert000033/convert.go
@@ -2,6 +2,7 @@ package convert000033
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	"go.uber.org/zap"
@@ -18,63 +19,79 @@ type ConvertWebFields struct {
 	convert.Basic
 }
 
-var webCompNames []string
-
 func (c *ConvertWebFields) Migrate() error {
 	if err := c.migratePipeline(); err != nil {
 		return err
 	}
+	return c.migratePipelineRelease()
+}
 
-	if err := c.migratePipelineRelease(); err != nil {
-		return err
-	}
-
-	if len(webCompNames) > 0 {
-		return fmt.Errorf("web components' output are not updated: %v", webCompNames)
-	}
-
-	return nil
+func (c *ConvertWebFields) getComponentTypeQuery(compType string) *gorm.DB {
+	pattern := fmt.Sprintf(`type:\s+%s`, compType)
+	return c.DB.Select("uid", "recipe_yaml", "recipe").
+		Where("recipe_yaml ~ ?", pattern).
+		Where("delete_time IS NULL")
 }
 
 func (c *ConvertWebFields) migratePipeline() error {
+	q := c.getComponentTypeQuery("web")
+
 	pipelines := make([]*datamodel.Pipeline, 0, batchSize)
-	return c.DB.Select("uid", "recipe_yaml", "recipe").FindInBatches(&pipelines, batchSize, func(tx *gorm.DB, _ int) error {
+	return q.FindInBatches(&pipelines, batchSize, func(tx *gorm.DB, _ int) error {
 		for _, p := range pipelines {
 			isRecipeUpdated := false
 			l := c.Logger.With(zap.String("pipelineUID", p.UID.String()))
 
-			if p.Recipe != nil {
-				for id, comp := range p.Recipe.Component {
-					isComponentUpdated, err := c.updateWebInput(id, comp)
-					if err != nil {
-						l.With(zap.String("componentID", id), zap.Error(err)).
-							Error("Failed to update pipeline.")
-
-						return fmt.Errorf("updating pipeline component: %w", err)
-					}
-
-					isRecipeUpdated = isComponentUpdated || isRecipeUpdated
-				}
+			if p.Recipe == nil {
+				continue
 			}
 
-			if isRecipeUpdated {
-				recipeYAML, err := yaml.Marshal(p.Recipe)
+			var webComponentNames []string
+
+			for id, comp := range p.Recipe.Component {
+				isComponentUpdated, err := c.updateWebInput(id, comp, &webComponentNames)
 				if err != nil {
-					return fmt.Errorf("marshalling recipe: %w", err)
+					l.With(zap.String("componentID", id), zap.Error(err)).
+						Error("Failed to update pipeline.")
+
+					return fmt.Errorf("updating pipeline component: %w", err)
 				}
 
-				updatedRecipe, err := c.updateWebOutputReceiver(recipeYAML)
+				isRecipeUpdated = isComponentUpdated || isRecipeUpdated
+			}
 
-				if err != nil {
-					return fmt.Errorf("updating pipeline output receiver: %w", err)
-				}
+			if !isRecipeUpdated {
+				continue
+			}
 
-				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", updatedRecipe)
+			recipeYAML, err := yaml.Marshal(p.Recipe)
+			if err != nil {
+				return fmt.Errorf("marshalling recipe: %w", err)
+			}
+
+			log.Printf("webComponentNames: %v", webComponentNames)
+
+			if len(webComponentNames) == 0 {
+				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", string(recipeYAML))
 				if result.Error != nil {
 					l.Error("Failed to update pipeline release.")
 					return fmt.Errorf("updating pipeline recipe: %w", result.Error)
 				}
+				continue
 			}
+
+			updatedRecipe, err := c.updateWebOutputReceiver(recipeYAML, webComponentNames)
+
+			if err != nil {
+				return fmt.Errorf("updating pipeline output receiver: %w", err)
+			}
+
+			result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", updatedRecipe)
+			if result.Error != nil {
+				l.Error("Failed to update pipeline release.")
+				return fmt.Errorf("updating pipeline recipe: %w", result.Error)
+			}
+
 		}
 
 		return nil
@@ -82,55 +99,73 @@ func (c *ConvertWebFields) migratePipeline() error {
 }
 
 func (c *ConvertWebFields) migratePipelineRelease() error {
+	q := c.getComponentTypeQuery("web")
+
 	pipelines := make([]*datamodel.Pipeline, 0, batchSize)
-	return c.DB.Select("uid", "recipe_yaml", "recipe").FindInBatches(&pipelines, batchSize, func(tx *gorm.DB, _ int) error {
+	return q.FindInBatches(&pipelines, batchSize, func(tx *gorm.DB, _ int) error {
 		for _, p := range pipelines {
 			isRecipeUpdated := false
 			l := c.Logger.With(zap.String("pipelineReleaseUID", p.UID.String()))
 
-			if p.Recipe != nil {
-				for id, comp := range p.Recipe.Component {
-					isComponentUpdated, err := c.updateWebInput(id, comp)
-					if err != nil {
-						l.With(zap.String("componentID", id), zap.Error(err)).
-							Error("Failed to update pipeline.")
-
-						return fmt.Errorf("updating pipeline component: %w", err)
-					}
-					isRecipeUpdated = isComponentUpdated || isRecipeUpdated
-				}
+			if p.Recipe == nil {
+				continue
 			}
 
-			if isRecipeUpdated {
-				recipeYAML, err := yaml.Marshal(p.Recipe)
+			var webComponentNames []string
+
+			for id, comp := range p.Recipe.Component {
+				isComponentUpdated, err := c.updateWebInput(id, comp, &webComponentNames)
 				if err != nil {
-					return fmt.Errorf("marshalling recipe: %w", err)
+					l.With(zap.String("componentID", id), zap.Error(err)).
+						Error("Failed to update pipeline.")
+
+					return fmt.Errorf("updating pipeline component: %w", err)
 				}
+				isRecipeUpdated = isComponentUpdated || isRecipeUpdated
+			}
 
-				updatedRecipe, err := c.updateWebOutputReceiver(recipeYAML)
+			if !isRecipeUpdated {
+				continue
+			}
 
-				if err != nil {
-					return fmt.Errorf("updating pipeline output receiver: %w", err)
-				}
+			recipeYAML, err := yaml.Marshal(p.Recipe)
+			if err != nil {
+				return fmt.Errorf("marshalling recipe: %w", err)
+			}
 
-				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", updatedRecipe)
+			if len(webComponentNames) == 0 {
+				result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", string(recipeYAML))
 				if result.Error != nil {
 					l.Error("Failed to update pipeline release.")
 					return fmt.Errorf("updating pipeline recipe: %w", result.Error)
 				}
+				continue
 			}
+
+			updatedRecipe, err := c.updateWebOutputReceiver(recipeYAML, webComponentNames)
+
+			if err != nil {
+				return fmt.Errorf("updating pipeline output receiver: %w", err)
+			}
+
+			result := tx.Model(p).Where("uid = ?", p.UID).Update("recipe_yaml", updatedRecipe)
+			if result.Error != nil {
+				l.Error("Failed to update pipeline release.")
+				return fmt.Errorf("updating pipeline recipe: %w", result.Error)
+			}
+
 		}
 
 		return nil
 	}).Error
 }
 
-func (c *ConvertWebFields) updateWebInput(compName string, comp *datamodel.Component) (bool, error) {
+func (c *ConvertWebFields) updateWebInput(compName string, comp *datamodel.Component, webComponentNames *[]string) (bool, error) {
 
 	if comp.Type == "iterator" {
 		isComponentUpdated := false
-		for _, comp := range comp.Component {
-			isUpdated, err := c.updateWebInput(compName, comp)
+		for compNameInIterator, comp := range comp.Component {
+			isUpdated, err := c.updateWebInput(compNameInIterator, comp, webComponentNames)
 			if err != nil {
 				return false, err
 			}
@@ -160,12 +195,26 @@ func (c *ConvertWebFields) updateWebInput(compName string, comp *datamodel.Compo
 		delete(input, "url")
 	}
 
-	webCompNames = append(webCompNames, compName)
+	timeout, timeoutFound := input["timeout"]
+
+	timeoutType := fmt.Sprintf("%T", timeout)
+
+	if timeoutFound && timeout != nil && timeoutType == "int" && timeout.(int) > 0 {
+		input["scrape-method"] = "chrome-simulator"
+	}
+
+	_, scrapeMethodFound := input["scrape-method"]
+
+	if !scrapeMethodFound {
+		input["scrape-method"] = "http"
+	}
+
+	*webComponentNames = append(*webComponentNames, compName)
 
 	return true, nil
 }
 
-func (c *ConvertWebFields) updateWebOutputReceiver(recipeYAML []byte) (string, error) {
+func (c *ConvertWebFields) updateWebOutputReceiver(recipeYAML []byte, webComponentNames []string) (string, error) {
 	originalFields := []string{
 		"content",
 		"markdown",
@@ -176,7 +225,7 @@ func (c *ConvertWebFields) updateWebOutputReceiver(recipeYAML []byte) (string, e
 	recipeString := string(recipeYAML)
 
 	updatedRecipe := recipeString
-	for _, compName := range webCompNames {
+	for _, compName := range webComponentNames {
 		for _, field := range originalFields {
 			// Don't have to add `}` because it could be `links-on-page[0]`
 			// It will be wrong if we add `}` at the end
@@ -187,9 +236,6 @@ func (c *ConvertWebFields) updateWebOutputReceiver(recipeYAML []byte) (string, e
 			updatedRecipe = strings.ReplaceAll(updatedRecipe, originalWebOutput, fmt.Sprintf("${%s.output.pages[0].%s", compName, field))
 		}
 	}
-
-	// Need to empty the slice after processing
-	webCompNames = []string{}
 
 	return updatedRecipe, nil
 }
