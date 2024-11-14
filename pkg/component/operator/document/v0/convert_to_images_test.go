f6456e3bd2ac5d967b3ceac41c1e5cae786988c7
ChunHao <64747455+chuang8511@users.noreply.github.com>
2024-11-14 04:16:24 +0000

    feat: convert pdf to image concurrently (#818)
    Because
    
    - it is slow when pdf to image
    
    This commit
    
    - make converter concurrently

---
+++ pkg/component/operator/document/v0/convert_to_images_test.go
@@ -0,0 +1,79 @@
+package document
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+
+	"github.com/instill-ai/pipeline-backend/pkg/component/base"
+	"github.com/instill-ai/pipeline-backend/pkg/component/internal/mock"
+	"github.com/instill-ai/pipeline-backend/pkg/data"
+	"github.com/instill-ai/pipeline-backend/pkg/data/format"
+)
+
+func Test_ConvertDocumentToImages(t *testing.T) {
+	c := qt.New(t)
+
+	test := struct {
+		name        string
+		filepath    string
+		expectedLen int
+	}{
+		name:        "Convert PDF to Images",
+		filepath:    "testdata/test.pdf",
+		expectedLen: 1,
+	}
+
+	bc := base.Component{}
+	ctx := context.Background()
+
+	component := Init(bc)
+	c.Assert(component, qt.IsNotNil)
+
+	execution, err := component.CreateExecution(base.ComponentExecution{
+		Component: component,
+		Task:      taskConvertToImages,
+	})
+	c.Assert(err, qt.IsNil)
+	c.Assert(execution, qt.IsNotNil)
+
+	fileContent, err := os.ReadFile(test.filepath)
+	c.Assert(err, qt.IsNil)
+
+	base64DataURI := fmt.Sprintf("data:%s;base64,%s", mimeTypeByExtension(test.filepath), base64.StdEncoding.EncodeToString(fileContent))
+
+	ir, ow, eh, job := mock.GenerateMockJob(c)
+
+	ir.ReadDataMock.Times(1).Set(func(ctx context.Context, input any) error {
+		switch input := input.(type) {
+		case *ConvertDocumentToImagesInput:
+			*input = ConvertDocumentToImagesInput{
+				Document: func() format.Document {
+					doc, err := data.NewDocumentFromURL(base64DataURI)
+					if err != nil {
+						return nil
+					}
+					return doc
+				}(),
+			}
+		}
+		return nil
+	})
+
+	ow.WriteDataMock.Times(1).Set(func(ctx context.Context, output any) error {
+		switch output := output.(type) {
+		case *ConvertDocumentToImagesOutput:
+			mock.Equal(len(output.Images), test.expectedLen)
+		}
+		return nil
+	})
+
+	eh.ErrorMock.Optional()
+
+	err = execution.Execute(ctx, []*base.Job{job})
+	c.Assert(err, qt.IsNil)
+}
--- pkg/component/operator/document/v0/transformer/execution/task_convert_to_images.py
+++ pkg/component/operator/document/v0/transformer/execution/task_convert_to_images.py
@@ -2,11 +2,11 @@ from io import BytesIO
 import json
 import base64
 import sys
+import pdfplumber
 
 # TODO chuang8511:
 # Deal with the import error when running the code in the docker container.
 # Now, we combine all python code into one file to avoid the import error.
-# from pdf_to_markdown import PDFTransformer
 # from pdf_to_markdown import PageImageProcessor
 
 if __name__ == "__main__":
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     params     = json.loads(json_str)
     filename   = params["filename"]
     pdf_string = params["PDF"]
+    page_idx   = params["page_idx"]
     if "resolution" in params and params["resolution"] != 0 and params["resolution"] != None:
         resolution = params["resolution"]
     else:
@@ -21,22 +22,19 @@ if __name__ == "__main__":
 
     decoded_bytes = base64.b64decode(pdf_string)
     pdf_file_obj = BytesIO(decoded_bytes)
-    pdf = PDFTransformer(x=pdf_file_obj)
-    pages = pdf.raw_pages
+    pdf = pdfplumber.open(pdf_file_obj)
+
+    page = pdf.pages[page_idx]
+
+    page_image = page.to_image(resolution=resolution)
+    encoded_image = PageImageProcessor.encode_image(page_image)
+
     exclude_file_extension = filename.split(".")[0]
-    filenames = []
-    images = []
-
-    for i, page in enumerate(pages):
-        page_image = page.to_image(resolution=resolution)
-        encoded_image = PageImageProcessor.encode_image(page_image)
-        images.append(encoded_image)
-        filenames.append(f"{exclude_file_extension}_{i}.png")
-
+    filename = f"{exclude_file_extension}_{page_idx}.png"
 
     output = {
-        "images": images,
-        "filename": filenames,
+        "image": encoded_image,
+        "filename": filename,
     }
 
     print(json.dumps(output))
