102e9c128876c3f8fad1b4452a8fb731ce627b1a
chuang8511 <a0961192312@gmail.com>
2024-11-20 18:22:59 +0000

    fix: fix time format

--- pkg/component/application/github/v0/issues.go
+++ pkg/component/application/github/v0/issues.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/go-github/v62/github"
 	"google.golang.org/protobuf/types/known/structpb"
@@ -97,8 +98,9 @@ func (githubClient *Client) listIssuesTask(ctx context.Context, props *structpb.
 	if err != nil {
 		return nil, err
 	}
-	// from format like `2006-01-02T15:04:05Z07:00` to time.Time
-	sinceTime, err := parseTime(inputStruct.Since)
+	// from format like `2006-01-02` parse it into UTC time
+	// The time will be 2006-01-02 00:00:00 +0000 UTC exactly
+	sinceTime, err := time.Parse(time.DateOnly, inputStruct.Since)
 	if err != nil {
 		return nil, err
 	}
@@ -106,7 +108,7 @@ func (githubClient *Client) listIssuesTask(ctx context.Context, props *structpb.
 		State:     inputStruct.State,
 		Sort:      inputStruct.Sort,
 		Direction: inputStruct.Direction,
-		Since:     *sinceTime,
+		Since:     sinceTime,
 		ListOptions: github.ListOptions{
 			Page:    inputStruct.Page,
 			PerPage: min(inputStruct.PerPage, 100), // GitHub API only allows 100 per page
--- pkg/component/application/github/v0/utils.go
+++ pkg/component/application/github/v0/utils.go
@@ -1,24 +1,6 @@
 package github
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/instill-ai/x/errmsg"
-)
-
 type PageOptions struct {
 	Page    int `json:"page"`
 	PerPage int `json:"per-page"`
-}
-
-func parseTime(since string) (*time.Time, error) {
-	sinceTime, err := time.Parse(time.RFC3339, since)
-	if err != nil {
-		return nil, errmsg.AddMessage(
-			fmt.Errorf("invalid time format"),
-			fmt.Sprintf("Cannot parse time: \"%s\". Please provide RFC3339 format(like %s)", since, time.RFC3339),
-		)
-	}
-	return &sinceTime, nil
 }
