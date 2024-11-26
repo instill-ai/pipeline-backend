24d049dbdca33816ff1227ea9f675d49021f2084
chuang8511 <a0961192312@gmail.com>
2024-11-26 13:47:28 +0000

    feat: implement the change of the design

--- pkg/component/application/leadiq/v0/io.go
+++ pkg/component/application/leadiq/v0/io.go
@@ -9,33 +9,54 @@ import (
 
 // FindProspectsInput is the input for the FindProspects task.
 type FindProspectsInput struct {
-	// CompanyName is the name of the company to find prospects for.
-	CompanyName string `instill:"company-name"`
+	// Company is the company information to find prospects for.
+	Company Company `instill:"company"`
 	// Limit is the maximum number of prospects to return.
 	Limit int `instill:"limit"`
 	// FilterBy is the filter to apply to the prospects.
 	FilterBy FilterBy `instill:"filter-by"`
-	// OrderBy is the order to sort the prospects by.
-	OrderBy OrderBy `instill:"order-by"`
+}
+
+// Company is the company information to find prospects for.
+type Company struct {
+	// Name is the name of the company.
+	Names []string `instill:"names,omitempty"`
+	// RevenueSize is the revenue size of the company.
+	RevenueSize RevenueSize `instill:"revenue-size,omitempty"`
+	// Countries is the list of countries the company operates in.
+	Countries []string `instill:"countries,omitempty"`
+	// States is the list of states the company operates in.
+	States []string `instill:"states,omitempty"`
+	// Cities is the list of cities the company operates in.
+	Cities []string `instill:"cities,omitempty"`
+	// Industries is the list of industries the company are.
+	Industries []string `instill:"industries,omitempty"`
+	// Descriptions is the list of descriptions of the company.
+	Descriptions []string `instill:"descriptions,omitempty"`
+	// Technologies is the list of technologies the company uses.
+	Technologies []string `instill:"technologies,omitempty"`
+}
+
+type RevenueSize struct {
+	// Min is the minimum revenue size.
+	Min int `instill:"min"`
+	// Max is the maximum revenue size.
+	Max int `instill:"max"`
+	// Description is the description of the revenue size.
+	Description string `instill:"description,omitempty"`
 }
 
 // FilterBy is the filter to apply to the prospects.
 type FilterBy struct {
 	// JobTitle is the job title to filter by.
 	JobTitle string `instill:"job-title,omitempty"`
-	// Seniority is the seniority to filter by.
-	Seniority string `instill:"seniority,omitempty"`
+	// Seniorities is the seniorities to filter by.
+	Seniorities []string `instill:"seniorities,omitempty"`
+	// Function is the function to filter by.
+	Function string `instill:"function,omitempty"`
 
-	jobTitleRegex  *regexp.Regexp
-	seniorityRegex *regexp.Regexp
-}
-
-// OrderBy is the order to sort the prospects by.
-type OrderBy struct {
-	// JobTitle is asc or desc to sort by job title.
-	JobTitle string `instill:"job-title,omitempty"`
-	// Seniority is asc or desc to sort by seniority.
-	Seniority string `instill:"seniority,omitempty"`
+	jobTitleRegex *regexp.Regexp
+	functionRegex *regexp.Regexp
 }
 
 // FindProspectsOutput is the output for the FindProspects task.
@@ -56,6 +77,20 @@ type Prospect struct {
 	Email string `instill:"email"`
 	// LinkedInURL is the LinkedIn URL of the prospect.
 	LinkedInURL string `instill:"linkedin-url"`
+	// CompanyName is the company name of the prospect.
+	CompanyName string `instill:"company-name"`
 	// CompanyDescription is the company description of the prospect.
 	CompanyDescription string `instill:"company-description"`
+	// CompanyIndustry is the company industry of the prospect.
+	CompanyIndustry string `instill:"company-industry"`
+	// CompanyAddress is the company address of the prospect.
+	CompanyAddress string `instill:"company-address"`
+	// CompanyTechnologies is the company technologies of the prospect.
+	CompanyTechnologies []string `instill:"company-technologies"`
+	// CompanyTechnologyCategories is the company technology categories of the prospect.
+	CompanyTechnologyCategories []string `instill:"company-technology-categories"`
+	// RevenueSize is the revenue size of the company of the prospect.
+	RevenueSize RevenueSize `instill:"revenue-size"`
+
+	function string
 }
--- pkg/component/application/leadiq/v0/task_find_prospects.go
+++ pkg/component/application/leadiq/v0/task_find_prospects.go
@@ -3,8 +3,9 @@ package leadiq
 import (
 	"context"
 	"fmt"
+	"log"
 	"regexp"
-	"sort"
+	"strings"
 	"time"
 
 	"github.com/machinebox/graphql"
@@ -15,9 +16,9 @@ import (
 )
 
 // 1. Fetch FlatAdvancedSearch from LeadIQ
-// 2. Filter prospects based on FilterBy
-// 3. Order prospects based on OrderBy
-// 4. Fetch email from searchPeople if workEmail is hidden
+// 2. Filter prospects based on FilterBy.Seniority and FilterBy.JobTitle
+// 3. Fetch email from searchPeople if workEmail is hidden
+// 4. Filter prospects based on FilterBy.Function
 func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) error {
 	logger := e.GetLogger()
 	client := newClient(e.GetSetup(), logger)
@@ -29,6 +30,8 @@ func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) err
 		return err
 	}
 
+	log.Println("====== inputStruct =======\n", inputStruct)
+
 	flatAdvancedSearchIn := buildFlatAdvancedSearchInput(inputStruct)
 	flatAdvancedSearchResp := flatAdvancedSearchResp{}
 	if err := e.sendRequest(ctx, client, logger, flatAdvancedSearchQuery, flatAdvancedSearchIn, &flatAdvancedSearchResp); err != nil {
@@ -38,7 +41,7 @@ func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) err
 		return err
 	}
 
-	prospects := make([]Prospect, 0, len(flatAdvancedSearchResp.FlatAdvancedSearch.People))
+	log.Println("====== flatAdvancedSearchResp =======\n", flatAdvancedSearchResp)
 
 	filterBy := inputStruct.FilterBy
 	if err := filterBy.compile(); err != nil {
@@ -47,13 +50,14 @@ func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) err
 		return err
 	}
 
+	prospects := make([]Prospect, 0, len(flatAdvancedSearchResp.FlatAdvancedSearch.People))
+
 	for _, person := range flatAdvancedSearchResp.FlatAdvancedSearch.People {
-		if !filterBy.match(person) {
+		if !filterBy.beforeAPICallingMatch(person) {
 			continue
 		}
 		// It is possible that the work email is hidden, so we need to find the first non-hidden email
 		// When workEmail is hidden, we should call searchPeople to get the email.
-		// When there is no workEmail, we don't need to call searchPeople.
 		var workEmail string
 		for _, email := range person.WorkEmails {
 			workEmail = email
@@ -63,39 +67,28 @@ func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) err
 		}
 
 		prospect := Prospect{
-			Name:               person.Name,
-			JobTitle:           person.Title,
-			Seniority:          person.Seniority,
-			Email:              workEmail,
-			LinkedInURL:        person.LinkedInURL,
-			CompanyDescription: person.Company.Description,
+			Name:                        person.Name,
+			JobTitle:                    person.Title,
+			Seniority:                   person.Seniority,
+			Email:                       workEmail,
+			LinkedInURL:                 person.LinkedInURL,
+			CompanyName:                 person.Company.Name,
+			CompanyDescription:          person.Company.Description,
+			CompanyIndustry:             person.Company.Industry,
+			CompanyAddress:              fmt.Sprintf("%s, %s, %s", person.Company.City, person.Company.State, person.Company.Country),
+			CompanyTechnologies:         person.Company.CompanyTechnologies,
+			CompanyTechnologyCategories: person.Company.CompanyTechnologyCategories,
+			RevenueSize: RevenueSize{
+				Min:         person.Company.RevenueRange.Start,
+				Max:         person.Company.RevenueRange.End,
+				Description: person.Company.RevenueRange.Description,
+			},
 		}
 		prospects = append(prospects, prospect)
 	}
 
-	orderBy := inputStruct.OrderBy
-	sort.Slice(prospects, func(i, j int) bool {
-		// Primary sort: JobTitle
-		if orderBy.JobTitle == "asc" && prospects[i].JobTitle != prospects[j].JobTitle {
-			return prospects[i].JobTitle < prospects[j].JobTitle
-		}
-		if orderBy.JobTitle == "desc" && prospects[i].JobTitle != prospects[j].JobTitle {
-			return prospects[i].JobTitle > prospects[j].JobTitle
-		}
-
-		// Secondary sort: Seniority
-		if orderBy.Seniority == "asc" {
-			return prospects[i].Seniority < prospects[j].Seniority
-		}
-		if orderBy.Seniority == "desc" {
-			return prospects[i].Seniority > prospects[j].Seniority
-		}
-		return false
-	})
-
 	for i := range prospects {
-		prospectName := prospects[i].Name
-		searchPeopleIn := buildSearchPeopleInput(inputStruct, prospectName)
+		searchPeopleIn := buildSearchPeopleInput(prospects[i])
 		searchPeopleResp := searchPeopleResp{}
 		if err := e.sendRequest(ctx, client, logger, searchPeopleQuery, searchPeopleIn, &searchPeopleResp); err != nil {
 			msg := fmt.Sprintf("LeadIQ API result error: %v", err)
@@ -105,14 +98,30 @@ func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) err
 		}
 
 		if !foundEmail(searchPeopleResp) {
-			continue
+			prospects[i].Email = ""
+		} else {
+			prospects[i].Email = searchPeopleResp.SearchPeople.Results[0].CurrentPositions[0].Emails[0].Value
 		}
 
-		prospects[i].Email = searchPeopleResp.SearchPeople.Results[0].CurrentPositions[0].Emails[0].Value
+		// Function is enum in LeadIQ. If they don't have the function, we set it to "Other", which is
+		// one of the Enum value.
+		if !foundFunction(searchPeopleResp) {
+			prospects[i].function = "Other"
+		} else {
+			prospects[i].function = searchPeopleResp.SearchPeople.Results[0].CurrentPositions[0].Function
+		}
+
+	}
+
+	filteredProspects := make([]Prospect, 0, len(prospects))
+	for _, prospect := range prospects {
+		if filterBy.afterAPICallingMatch(prospect) {
+			filteredProspects = append(filteredProspects, prospect)
+		}
 	}
 
 	output := FindProspectsOutput{
-		Prospects: prospects,
+		Prospects: filteredProspects,
 	}
 
 	if err := job.Output.WriteData(ctx, &output); err != nil {
@@ -127,7 +136,8 @@ func (e *execution) executeFindProspects(ctx context.Context, job *base.Job) err
 func (e *execution) sendRequest(ctx context.Context, client *graphql.Client, logger *zap.Logger, query string, input map[string]interface{}, output any) error {
 	req := graphql.NewRequest(query)
 
-	// Set API Key. graphql.NewRequest does not support setting headers.
+	// graphql.NewRequest does not support setting headers.
+	// So, we have to set the headers here.
 	req.Header.Set("Authorization", "Basic "+getAPIKey(e.GetSetup()))
 
 	req.Var("input", input)
@@ -180,19 +190,77 @@ func (e *execution) sendRequest(ctx context.Context, client *graphql.Client, log
 }
 
 func buildFlatAdvancedSearchInput(in FindProspectsInput) map[string]interface{} {
-	return map[string]interface{}{
-		"companyFilter": map[string]interface{}{
-			"names": []string{in.CompanyName},
-		},
-		"limit": in.Limit,
+	companyFilter := make(map[string]interface{})
+
+	// Now, we don't support optional fields in the Console/API input.
+	// So, we set the default value with the " " string.
+	// If it is the default value, it means the users don't provide any information.
+	// So, we won't add the field to the companyFilter.
+	filterNonBlank := func(slice []string) []string {
+		result := []string{}
+		for _, s := range slice {
+			trimmed := strings.TrimSpace(s)
+			if trimmed != "" {
+				result = append(result, trimmed)
+			}
+		}
+		return result
 	}
+
+	names := filterNonBlank(in.Company.Names)
+	if len(names) > 0 {
+		companyFilter["names"] = names
+	}
+
+	// If there are names and revenue size at the same time, LeadIQ won't return any result. So, we will ignore the revenue size when the users provide the names.
+	if len(names) == 0 && (in.Company.RevenueSize.Min != 0 || in.Company.RevenueSize.Max != 0) {
+		revenueRanges := map[string]interface{}{}
+		if in.Company.RevenueSize.Min != 0 {
+			revenueRanges["start"] = in.Company.RevenueSize.Min
+		}
+		if in.Company.RevenueSize.Max != 0 {
+			revenueRanges["end"] = in.Company.RevenueSize.Max
+		}
+		companyFilter["revenueRanges"] = revenueRanges
+	}
+
+	if countries := filterNonBlank(in.Company.Countries); len(countries) > 0 {
+		companyFilter["countries"] = countries
+	}
+
+	if states := filterNonBlank(in.Company.States); len(states) > 0 {
+		companyFilter["states"] = states
+	}
+
+	if cities := filterNonBlank(in.Company.Cities); len(cities) > 0 {
+		companyFilter["cities"] = cities
+	}
+
+	if industries := filterNonBlank(in.Company.Industries); len(industries) > 0 {
+		companyFilter["industries"] = industries
+	}
+
+	if descriptions := filterNonBlank(in.Company.Descriptions); len(descriptions) > 0 {
+		companyFilter["descriptions"] = descriptions
+	}
+
+	if technologies := filterNonBlank(in.Company.Technologies); len(technologies) > 0 {
+		companyFilter["technologies"] = technologies
+	}
+
+	builtInput := map[string]interface{}{
+		"companyFilter": companyFilter,
+		"limit":         in.Limit,
+	}
+
+	return builtInput
 }
 
-func buildSearchPeopleInput(in FindProspectsInput, prospectName string) map[string]interface{} {
+func buildSearchPeopleInput(prospect Prospect) map[string]interface{} {
 	return map[string]interface{}{
-		"fullName": prospectName,
+		"fullName": prospect.Name,
 		"company": map[string]string{
-			"name": in.CompanyName,
+			"name": prospect.CompanyName,
 		},
 	}
 }
@@ -205,8 +273,8 @@ func (f *FilterBy) compile() error {
 			return fmt.Errorf("compiling job title filter: %w", err)
 		}
 	}
-	if f.Seniority != "" {
-		f.seniorityRegex, err = regexp.Compile(f.Seniority)
+	if f.Function != "" {
+		f.functionRegex, err = regexp.Compile(f.Function)
 		if err != nil {
 			return fmt.Errorf("compiling seniority filter: %w", err)
 		}
@@ -214,11 +282,27 @@ func (f *FilterBy) compile() error {
 	return nil
 }
 
-func (f *FilterBy) match(person person) bool {
+func (f *FilterBy) beforeAPICallingMatch(person person) bool {
 	if f.jobTitleRegex != nil && !f.jobTitleRegex.MatchString(person.Title) {
 		return false
 	}
-	if f.seniorityRegex != nil && !f.seniorityRegex.MatchString(person.Seniority) {
+	if len(f.Seniorities) > 0 {
+		found := false
+		for _, seniority := range f.Seniorities {
+			if seniority == person.Seniority {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func (f *FilterBy) afterAPICallingMatch(prospect Prospect) bool {
+	if f.functionRegex != nil && !f.functionRegex.MatchString(prospect.function) {
 		return false
 	}
 	return true
@@ -243,6 +327,21 @@ func foundEmail(searchPeopleResp searchPeopleResp) bool {
 	return email.Status == "Verified"
 }
 
+func foundFunction(searchPeopleResp searchPeopleResp) bool {
+	if len(searchPeopleResp.SearchPeople.Results) == 0 {
+		return false
+	}
+
+	result := searchPeopleResp.SearchPeople.Results[0]
+	if len(result.CurrentPositions) == 0 {
+		return false
+	}
+
+	function := result.CurrentPositions[0].Function
+
+	return function != ""
+}
+
 // flatAdvancedSearchResp is the response from the flatAdvancedSearch query.
 // We only extract the fields we need.
 type flatAdvancedSearchResp struct {
@@ -263,6 +362,20 @@ type person struct {
 }
 
 type company struct {
+	Name                        string       `json:"name"`
+	Description                 string       `json:"description"`
+	Industry                    string       `json:"industry"`
+	Country                     string       `json:"country"`
+	State                       string       `json:"state"`
+	City                        string       `json:"city"`
+	CompanyTechnologies         []string     `json:"companyTechnologies"`
+	CompanyTechnologyCategories []string     `json:"companyTechnologyCategories"`
+	RevenueRange                revenueRange `json:"revenueRange"`
+}
+
+type revenueRange struct {
+	Start       int    `json:"start"`
+	End         int    `json:"end"`
 	Description string `json:"description"`
 }
 
@@ -287,7 +400,8 @@ type currentPosition struct {
 	CompanyInfo struct {
 		Name string `json:"name"`
 	} `json:"companyInfo"`
-	Emails []email `json:"emails"`
+	Emails   []email `json:"emails"`
+	Function string  `json:"function"`
 }
 
 type email struct {
