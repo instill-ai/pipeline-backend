06c8025ecc35774cf3cc2959546cb42c5c49caab
Juan Vallés <3977183+jvallesm@users.noreply.github.com>
2025-09-18 16:28:39 +0200

    fix(usage): treat input rendering error as fatal (#1098)
    Because
    
    - Usage handler error produces a misleading error when the input data
      fails to be rendered.
    
    This commit
    
    - Fixes INS-8583
    - Passes only the valid inputs to the usage checker.
    - Returns a fatal error if no valid inputs could be rendered.

--- pkg/component/ai/instillmodel/v0/main_test.go
+++ pkg/component/ai/instillmodel/v0/main_test.go
@@ -145,7 +145,7 @@ func TestExecution_Execute(t *testing.T) {
 
 		// Create mock job
 		input, _ := structpb.NewStruct(map[string]any{
-			"model-name": "test-model",
+			"model-name": "namespace/test-model/v0.1.0",
 			"prompt":     "test prompt",
 		})
 
